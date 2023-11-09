5f51b98d24831dc2bb17ead0efc2b26bbcd906c3
Asaromi <a.sabililromi@gmail.com>
2023-11-09 19:09:17 +0700

    fix: model - add role relation to responsibilities

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -16,8 +16,23 @@ class Role extends Model
      */
     protected $fillable = ['name', 'company_id'];
 
+    /**
+     * Get the company that owns the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function company()
     {
         return $this->belongsTo(Company::class, 'company_id');
+    }
+
+    /**
+     * Get all of the responsibilities for the Role
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function responsibilities()
+    {
+        return $this->hasMany(Responsibility::class, 'role_id');
     }
 }
