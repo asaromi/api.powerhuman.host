844116fcaed222d48aec9d790e8faeb993cb8366
Asaromi <a.sabililromi@gmail.com>
2023-11-11 05:22:15 +0700

    fix: add condition response

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -32,9 +32,26 @@ class ResponseFormatter
             self::$response['meta']['message'] = $message;
         }
 
-        self::$response['meta']['success'] = false;
-        self::$response['meta']['code'] = $code ?? 500;
+        $code = (int) $code;
+        if ($code < 400) $code = 400;
+        else if ($code > 505) $code = 500;
+
+        self::$response['meta']= [
+            'code' => $code,
+            'success' => false
+        ];
         self::$response['result'] = $result;
         return response(self::$response, $code);
+    }
+
+    public function notFound()
+    {
+        self::$response['meta'] = [
+            'code' => 404,
+            'success' => false,
+            'message' => "Endpoint Not Found"
+        ];
+
+        return response(self::$response, 404);
     }
 }
