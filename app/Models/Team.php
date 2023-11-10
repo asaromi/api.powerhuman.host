417e179a0160ec972c2daf0f8d59160dafcbdab4
Asaromi <a.sabililromi@gmail.com>
2023-11-11 05:20:44 +0700

    fix: model - add user relation

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -27,12 +27,22 @@ class Team extends Model
     }
 
     /**
-     * Get all team's employee
+     * Get all Team's employee
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function employees()
     {
         return $this->hasMany(Employee::class, 'team_id');
+    }
+
+    /**
+     * Get all of the Team's user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class, 'current_team_id');
     }
 }
